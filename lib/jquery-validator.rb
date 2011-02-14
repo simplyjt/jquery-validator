63676490b90e5b491df46f193b09e7fd51f54ad9
pehrlich <peter.i.ehrlich@gmail.com>
2011-02-14 09:16:14 +0700

    including rest of files, renamed test

--- lib/jquery-validator.rb
+++ lib/jquery-validator.rb
@@ -1,5 +1,2 @@
 require 'jquery_validator/base'
 require 'jquery_validator/form_builder'
-
-#require File.join(File.dirname(__FILE__), *%w[jquery_validator base])
-#require File.join(File.dirname(__FILE__), *%w[jquery_validator form_builder])
--- lib/jquery_validator/form_builder.rb
+++ lib/jquery_validator/form_builder.rb
@@ -2,21 +2,24 @@ module ActionView
   module Helpers
     class FormBuilder
       def jquery_validators(options = {})
-          fields = options.delete(:only)
+          selected_fields = options.delete(:only)
           
           @object.class.validators.each do |v|
-            if !fields || fields.include?(v.attributes.first)
-              if validator = JqueryValidator.factory(v, self)
-                options.deep_merge!(JqueryValidator.factory(v, self).validate_arguments)
+              if validator = JqueryValidator.factory(v, self, selected_fields) #check if matching validator exists
+                options.deep_merge!(validator.validate_attributes) 
               end
-            end
           end
+                    
+          # determines form name; copied from rails source: FormHelper::apply_form_for_options!
+          action = (@object.respond_to?(:persisted?) && @object.persisted?) ? :edit : :new
+          dom_id = @options[:as] ? "#{@options[:as]}_#{action}" : ActionController::RecordIdentifier.dom_id(@object, action)
+          
           js = options.to_json
           js.gsub!(/"\s*(function\(.*\})\s*\"/, '\1')
           code = %Q[
             <script>
-            $(document).ready(function() {
-              $("##{ActionController::RecordIdentifier.dom_id(@object, @object.new_record? ? :new : :edit)}").validate(#{js});
+            $(document).ready(function() {                                        
+              $("##{dom_id}").validate(#{js});
             });
             </script>
           ]
