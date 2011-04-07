3e924d70c153733265617df03c144d5fbe1d9c5b
JT <jt@simplyjt.com>
2011-04-07 11:56:46 -0700

    fix case where one validator can have multiple attributes i.e. validates_presences_of :first_name, :last_name

--- lib/jquery_validator/form_builder.rb
+++ lib/jquery_validator/form_builder.rb
@@ -2,25 +2,35 @@ module ActionView
   module Helpers
     class FormBuilder
       def jquery_validators(options = {})
-          fields = options.delete(:only)
-          
-          @object.class.validators.each do |v|
-            if !fields || fields.include?(v.attributes.first)
-              if validator = JqueryValidator.factory(v, self)
-                options.deep_merge!(JqueryValidator.factory(v, self).validate_arguments)
-              end
+        fields = options.delete(:only)
+        v_arr = @object.class.validators.map do |v|
+          if v.attributes.size > 1
+            v.attributes.map do |a|
+              new_v = v.clone
+              new_v.instance_variable_set "@attributes", [a]
+              new_v
+            end
+          else
+            v
+          end
+        end
+        v_arr.flatten.each do |v|
+          if !fields || fields.include?(v.attributes.first)
+            if validator = JqueryValidator.factory(v, self)
+              options.deep_merge!(JqueryValidator.factory(v, self).validate_arguments)
             end
           end
-          js = options.to_json
-          js.gsub!(/"\s*(function\(.*\})\s*\"/, '\1')
-          code = %Q[
+        end
+        js = options.to_json
+        js.gsub!(/"\s*(function\(.*\})\s*\"/, '\1')
+        code = %Q[
             <script>
             $(document).ready(function() {
               $("##{ActionController::RecordIdentifier.dom_id(@object, @object.new_record? ? :new : :edit)}").validate(#{js});
             });
             </script>
           ]
-          code.html_safe
+        code.html_safe
       end
     end
   end
